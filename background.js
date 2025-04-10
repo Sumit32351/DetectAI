8eca73453d7010b77e888648079a9a868b105a97
Skullcrushe98 <148679157+Skullcrushe98@users.noreply.github.com>
2025-04-10 20:46:33 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
-
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url && tab.url.includes("*://stackoverflow.com/*")) {
+        chrome.action.setPopup({
+            tabId: tabId,
+            popup: "popup/popup.html"
+        });
+    } else {
+        chrome.action.setPopup({
+            tabId: tabId,
+            popup: ""
+        });
+    }
+});
